c7ed029403df9f20a64e979acfca30e332b01674
drDramen <vvpalchin@gmail.com>
2022-12-20 16:49:35 +0200

    refactor: remove "vars" varible

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { vars } from '@/varibles/varibles';
 import '@/assets/styles/index.scss';
 
-vars.htmlEl.classList.add('page');
-vars.bodyEl.classList.add('page__body');
+document.documentElement.classList.add('page');
+document.body.classList.add('page__body');
--- src/varibles/varibles.ts
+++
@@ -1,6 +0,0 @@
-export const vars = {
-  windowEl: window,
-  documentEl: document,
-  htmlEl: document.documentElement,
-  bodyEl: document.body,
-};
