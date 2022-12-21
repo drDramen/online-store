89a226a4669f4dd1b5ceb8c56de707fad624cc1c
lyutails <lyusya.cool@gmail.com>
2022-12-21 18:38:07 +0300

    fix: correct fields naming in data array plus correct product images import

--- src/interfaces/product.ts
+++ src/interfaces/product.ts
@@ -1,16 +1,15 @@
 export interface Product {
-  id: number;
-  brand_name: string;
-  product_name: string;
-  description: string,
-  ingredients: string,
-  star_ingredient: string,
-  pic_1: string,
-  pic_2: string,
-  price: number,
-  bestseller: boolean,
-  segment: string,
-  category: string,
-  for: string,
-  available_amount: number,
+  id: string;
+  brandName: string;
+  productName: string;
+  description: string;
+  ingredients: string;
+  starIngredient: string;
+  pics: string[];
+  price: number;
+  bestseller: boolean;
+  segment: string;
+  category: string;
+  for: string;
+  availableAmount: number;
 }
