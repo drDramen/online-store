e0213ddeea88e319b12c95372e58fefd2dc9b16f
lyutails <lyusya.cool@gmail.com>
2022-12-21 20:05:21 +0300

    feat: start app header

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       title: 'Online-store',
-      favicon: paths.public + '/favicon/favicon.png',
+      favicon: paths.public + '/favicon/favicon_2.svg',
       template: paths.src + '/template.html',
       filename: 'index.html',
     }),
