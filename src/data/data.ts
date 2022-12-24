101d4daee2335be0f291d2bcfbfe9e381a110195
lyutails <lyusya.cool@gmail.com>
2022-12-24 04:26:38 +0300

    feat: fill five bioderma positions

--- src/data/data.ts
+++ src/data/data.ts
@@ -42,4 +42,146 @@ export const products: Product[] = [
     for: 'Face',
     availableAmount: 200,
   },
+  {
+    id: '0100',
+    brandName: 'Bioderma',
+    productName: 'Sensibio Gel Moussant',
+    description: `Transparent gel Bioderma Sensibio Gel Moussant delicately
+     cleanses sensitive skin of the face from dirt, makeup, excess sebum.
+      Thanks to its neutral pH, soothing and softening ingredients, it is
+       suitable for the eye area. Patented D.A.F. (Dermatological Advanced 
+        Formulation) lowers the reactivity threshold.
+    After washing with Bioderma Sensibio Gel Moussant, there are no such 
+    problems as redness and tightness of the skin. The face looks fresh and rested.`,
+    ingredients: `Aqua/Water/Eau, Sodium Cocoamphoacetate, Propanediol, 
+    Sodium Lauroyl Sarcosinate, Citric Acid, Coco-Glucoside, 
+    Glyceryl Oleate, Sodium Citrate, Peg-90 Glyceryl Isostearate, 
+    Mannitol, Xylitol, Laureth-2, Rhamnose, Fructooligosaccharides, 
+    Tocopherol, Hydrogenated Palm Glycerides Citrate, Lecithin, 
+    Ascorbyl Palmitate`,
+    starIngredient: '',
+    pics: [
+      'assets/product-images/0100/bioderma-gel-muss-sensibio-1.jpg',
+      'assets/product-images/0100/bioderma-gel-muss-sensibio-2.jpg',
+    ],
+    price: 28,
+    bestseller: true,
+    segment: 'Drugstore (Europe)',
+    category: 'Cleanser',
+    for: 'Face',
+    availableAmount: 180,
+  },
+  {
+    id: '0101',
+    brandName: 'Bioderma',
+    productName: 'Sensibio Micelle Solution',
+    description: `Bioderma Sensibio Micelle solution
+     (Bioderma Sensibio H2O Cleansing Water) for fragile, thin, sensitive 
+     skin prone to skin redness. Micellar solution Sensibio H20 qualitatively 
+     and quickly cleanses the skin of the face and eye contour by microemulsifying 
+     impurities and makeup, while maintaining the hydrolipidic balance of 
+     the stratum corneum of the skin. Soothes and moisturizes the skin. 
+     Does not contain alkali, alcohol, phenoxyethanol and fragrances; pH neutral.`,
+    ingredients: `Aqua/water/eau, Peg-6 Caprylic/capric Glycerides,
+     Fructooligosaccharides, Mannitol, Xylitol, Rhamnose, Cucumis Sativus
+      (cucumber) Fruit Extract, Propylene Glycol, Cetrimonium Bromide, Disodium Edta`,
+    starIngredient: '',
+    pics: [
+      'assets/product-images/0101/bioderma-sensibio-h2o-makeup-removing-micelle-solution-1.jpg',
+      'assets/product-images/0101/bioderma-sensibio-h2o-makeup-removing-micelle-solution-2.jpg',
+    ],
+    price: 32,
+    bestseller: true,
+    segment: 'Drugstore (Europe)',
+    category: 'Cleanser',
+    for: 'Face',
+    availableAmount: 110,
+  },
+  {
+    id: '0102',
+    brandName: 'Bioderma',
+    productName: 'Sébium Gel moussant actif',
+    description: `Especially for the cleansing of oily skin prone to acne, 
+    the Bioderma laboratory offers a solution with an optimal combination
+     of keratolytic effect and good tolerance - Intensive Action Gel Active.
+     The soap-free formula with zinc gluconate, salicylic acid and glycolic
+      acid has a physiological pH level, deeply cleanses pores, improves
+       skin texture, evens out tone and eliminates shine. Bioderma Sébium
+        Gel moussant actif, during the cleansing phase, acts on both
+         the causes of acne and the consequences.`,
+    ingredients: `Aqua/Water/Eau, Caprylyl/Capryl Glucoside, Propanediol, Glycerin,
+     Zinc Gluconate, Salicylic Acid, Glycolic Acid, Coco-Glucoside, Glyceryl Oleate,
+      Cellulose Gum, Sodium Hydroxide, Citric Acid, Fragrance (Perfume), Inulin,
+       Xanthan Gum, Mannitol, Sodium Citrate, Xylitol, Cellulose, Fructose,
+        Glucose, Rhamnosis, Sodium Metabisulphite, Fructooligosaccharides,
+         Tocopherol, Hydrogenated Palm Glycerides Citrate`,
+    starIngredient: '',
+    pics: [
+      'assets/product-images/0102/bioderma-sébium-gel-moussant-actif-1.jpg',
+      'assets/product-images/0102/bioderma-sébium-gel-moussant-actif-2.jpg',
+    ],
+    price: 22,
+    bestseller: false,
+    segment: 'Drugstore (Europe)',
+    category: 'Cleanser',
+    for: 'Face',
+    availableAmount: 118,
+  },
+  {
+    id: '0103',
+    brandName: 'Bioderma',
+    productName: 'Sebium purifying foaming gel',
+    description: `Sébium Foaming Gel gently cleanses and purifies the skin without
+     drying it : containing zinc and copper sulphates, it cleanses the epidermis,
+      reduces the appearance of irregularities, and absorbs excess of sebum.
+       Its ultra-gentle, soap-free formula respects the skin's balance and
+        its physiological pH is maintained. The patented natural complex
+         contributes to raise the skin's tolerance threshold.`,
+    ingredients: `Water (Aqua), Sodium Cocoamphoacetate, Sodium Laureth Sulfate,
+     PEG-200 Hydrogenated Glyceryl Palmate, PEG-40 Glyceryl Cocoate, Disodium EDTA,
+      Citric Acid, Zinc Sulfate, Sodium Coceth Sulfate, PEG-7 Glyceryl Cocoate,
+       Copper Sulfate, Ginkgo Biloba Extract, Sodium Chloride, Xylitol,
+        Fructooligosaccharides, Mannitol, Rhamnose, Sodium Hydroxide,
+         Propylene Glycol, Sodium Methylparaben, Sodium Propylparaben, Fragrance (Parfum)`,
+    starIngredient: '',
+    pics: [
+      'assets/product-images/0103/bioderma-sébium-gel-moussant-actif-1.jpg',
+      'assets/product-images/0103/bioderma-sébium-gel-moussant-actif-2.jpg',
+    ],
+    price: 23,
+    bestseller: false,
+    segment: 'Drugstore (Europe)',
+    category: 'Cleanser',
+    for: 'Face',
+    availableAmount: 220,
+  },
+  {
+    id: '0104',
+    brandName: 'Bioderma',
+    productName: 'Hydrabio water Micelle solution',
+    description: `The high efficiency and tolerance of micellar water Bioderma
+     Hydrabio H2O is provided with micelles of fatty acid esters, which are
+      identical to the phospholipids of skin cell membranes. Micelles restore
+       the hydrolipidic film of the epidermis, effectively cleanse the skin
+        even from waterproof cosmetics without the appearance of signs of
+         discomfort and dryness. Bioderma Hydrabio water Micelle solution
+          is suitable for removing sebum, cosmetics and impurities without rinsing with water.`,
+    ingredients: `Aqua/water/eau, Glycerin, Peg-6 Caprylic/capric Glycerides,
+     Disodium Edta, Mannitol, Xylitol, Cetrimonium Bromide, Rhamnose, Niacinamide,
+      Hexyldecanol, Sodium Hydroxide, Pyrus Malus (apple) Seed Extract,
+       Brassica Campestris (rapeseed) Sterols, Tocopherol, Fragrance (parfum)`,
+    starIngredient: '',
+    pics: [
+      'assets/product-images/0104/bioderma-hydrabio-water-micelle-solution-1.jpg',
+      'assets/product-images/0104/bioderma-hydrabio-water-micelle-solution-2.jpg',
+      'assets/product-images/0104/bioderma-hydrabio-water-micelle-solution-3.jpg',
+      'assets/product-images/0104/bioderma-hydrabio-water-micelle-solution-4.jpg',
+    ],
+    price: 30,
+    bestseller: false,
+    segment: 'Drugstore (Europe)',
+    category: 'Cleanser',
+    for: 'Face',
+    availableAmount: 220,
+  },
 ];
