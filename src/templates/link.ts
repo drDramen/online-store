c35a32264962445567d6e883b32124199472dd40
drDramen <vvpalchin@gmail.com>
2022-12-27 12:37:24 +0200

    feat: create Link component

---
+++ src/templates/link.ts
@@ -0,0 +1,36 @@
+import { Router } from '@/router/router';
+import { BaseComponent } from './base-component';
+
+export class Link extends BaseComponent {
+  private static navigationLinks: Link[] = [];
+
+  constructor(props: Partial<HTMLAnchorElement>) {
+    super('a', props);
+    this.node.addEventListener('click', this.changeRoute);
+  }
+
+  private changeRoute = (event: Event) => {
+    event.preventDefault();
+    const { currentTarget } = event;
+    if (currentTarget instanceof HTMLAnchorElement) {
+      window.history.pushState({}, '', currentTarget.href);
+      Link.changeActive();
+      Router.onPathChangeHandler();
+    }
+  };
+
+  static addNavigationLink(navigationLink: Link) {
+    this.navigationLinks.push(navigationLink);
+  }
+
+  static changeActive() {
+    const path = window.location.pathname;
+    Link.navigationLinks.forEach((link) => {
+      if (link.getNode().getAttribute('href') === path) {
+        link.addClass('active');
+      } else {
+        link.removeClass('active');
+      }
+    });
+  }
+}
