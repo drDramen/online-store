8f193dcff5a6f47b6fe9871cc44d9c6f40e14989
lyutails <lyusya.cool@gmail.com>
2022-12-27 17:48:13 +0300

    feat: correct logo hover

--- src/components/logo/logo.ts
+++ src/components/logo/logo.ts
@@ -4,8 +4,6 @@ import './logo.scss';
 export class Logo extends BaseComponent {
   private logoImage: BaseComponent;
   private logoText: BaseComponent;
-  private logoTextFirst: BaseComponent;
-  private logoTextSecond: BaseComponent;
 
   constructor() {
     super('div', {
@@ -16,13 +14,7 @@ export class Logo extends BaseComponent {
     this.append(this.logoImage);
 
     this.logoText = new BaseComponent('div', { className: 'logo__text' }, this.node);
-
-    this.logoTextFirst = new BaseComponent('span', { className: 'logo_colour_one' }, this.node);
-    this.logoTextFirst.setInnerHTML('Take');
-    this.logoText.append(this.logoTextFirst);
-
-    this.logoTextSecond = new BaseComponent('span', { className: 'logo_colour_two' }, this.node);
-    this.logoTextSecond.setInnerHTML('Care');
-    this.logoText.append(this.logoTextSecond);
+    this.logoText.setInnerHTML('TakeCare');
+    this.append(this.logoText);
   }
 }
