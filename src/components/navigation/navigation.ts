888b4dd1aa97195fcc8dcaebf9b19cdaf181e3ee
drDramen <vvpalchin@gmail.com>
2022-12-27 12:41:39 +0200

    refactor: adapt the Navigation component to use Router

--- src/components/navigation/navigation.ts
+++ src/components/navigation/navigation.ts
@@ -1,60 +1,40 @@
-import { AppRoute } from './../enums/enums';
+import { NameRoute } from '@/enums/name-route';
 import { BaseComponent } from '@/templates/base-component';
+import { Link } from '@/templates/link';
 import './navigation.scss';
 
 const navigationLinks = [
-  { text: 'Home', href: AppRoute.Home },
-  { text: 'Shop', href: AppRoute.Shop },
-  { text: 'Contacts', href: AppRoute.Contacts },
+  { textContent: 'Home', href: NameRoute.Home },
+  { textContent: 'Shop', href: NameRoute.Catalog },
+  { textContent: 'Contacts', href: NameRoute.Contacts },
 ];
 
 export class Navigation extends BaseComponent {
-  private navigationItems: BaseComponent;
+  private navigationList: BaseComponent;
 
   constructor() {
     super('nav', {
       className: 'navigation',
     });
 
-    this.navigationItems = new BaseComponent('ul', { className: 'navigation__items' });
-    this.append(this.navigationItems);
+    this.navigationList = new BaseComponent('ul', { className: 'navigation__items list-reset' });
+    this.append(this.navigationList);
 
     this.createNavigationItems();
+    Link.changeActive();
   }
 
-  onPathChange() {
-    const path = window.location.pathname;
-    console.log(path);
-  }
-
-  createNavigationItems() {
-    navigationLinks.forEach(({ text, href }) => {
-      const navigationItem = new BaseComponent('li', { className: 'navigation__item' });
-      const navigationLink = new BaseComponent('a', {
+  private createNavigationItems() {
+    navigationLinks.forEach((props) => {
+      const navItem = new BaseComponent('li', { className: 'navigation__item' });
+      const navLink = new Link({
+        ...props,
         className: 'navigation__link',
-        href: href,
-        textContent: text,
       });
-      navigationLink.getNode().addEventListener('click', (event: Event) => {
-        event.preventDefault();
-        window.history.pushState({}, '', href);
-        this.onPathChange();
-        this.navigationItems.getChildren().forEach((element) => {
-          element.removeClass('active');
-        });
-        const { currentTarget } = event;
-        if (currentTarget instanceof HTMLElement) {
-          //currentTarget.classList.add('active');
-          currentTarget.parentElement?.classList.add('active');
-        }
-      });
-      navigationItem.append(navigationLink);
-      this.navigationItems.append(navigationItem);
-    });
 
-    //window.addEventListener('popstate', this.onPathChange.bind(this));
-    window.addEventListener('popstate', () => {
-      this.onPathChange();
+      Link.addNavigationLink(navLink);
+      navItem.append(navLink);
+      this.navigationList.append(navItem);
     });
   }
 }
