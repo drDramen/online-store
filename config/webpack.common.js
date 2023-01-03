24be5005585b600470d9acba2b7cbad47952255c
lyutails <lyusya.cool@gmail.com>
2023-01-04 00:11:55 +0300

    fix: add additional extension for image

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|avif)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'assets/img/[hash][ext]',
