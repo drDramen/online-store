e8f31b587e02d47bbab230be2198b0f02395371c
lyutails <lyusya.cool@gmail.com>
2023-01-05 20:40:44 +0300

    feat: append top menu to favourites page

--- src/components/pages/favourites-page/favourites-page.ts
+++ src/components/pages/favourites-page/favourites-page.ts
@@ -1,12 +1,16 @@
+import { TopMenu } from '@/components/top-menu/top-menu';
 import { Container } from '@/components/container/container';
 import { BaseComponent } from '@/templates/base-component';
 import './favourites-page.scss';
 
 export class FavouritesPage extends BaseComponent {
   container: Container;
+  private topMenu: TopMenu = new TopMenu();
 
   constructor() {
     super('div', { className: 'favourites' });
+
+    this.append(this.topMenu);
 
     this.container = new Container('favs__container');
     this.append(this.container);
