6ae53f4a2eea706b8a9ca3572b6267c38f33c7e9
lyutails <lyusya.cool@gmail.com>
2023-01-05 21:13:13 +0300

    feat: add top menu to brands page

--- src/components/pages/brands-page/brands-page.ts
+++ src/components/pages/brands-page/brands-page.ts
@@ -1,12 +1,16 @@
+import { TopMenu } from '@/components/top-menu/top-menu';
 import { Container } from '@/components/container/container';
 import { BaseComponent } from '@/templates/base-component';
 import './brands-page.scss';
 
 export class BrandsPage extends BaseComponent {
   private container: Container;
+  private topMenu: TopMenu = new TopMenu();
 
   constructor() {
     super('div', { className: 'brands' });
+
+    this.append(this.topMenu);
 
     this.container = new Container('brands__container');
     this.container.setInnerHTML('BrandsPage'); // TEMPORARY
