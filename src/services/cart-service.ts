c6ed44d1f8352715663aa84fc536cc75d5de3001
drDramen <vvpalchin@gmail.com>
2023-01-09 22:07:37 +0200

    feat: start implements cart service

---
+++ src/services/cart-service.ts
@@ -0,0 +1,23 @@
+import { Product } from '@/interfaces/product';
+
+export class CartService {
+  private static cart: { product: Product; count: number }[] = [];
+
+  static addToCart(product: Product) {
+    const productInCart = this.cart.find(({ product: prod }) => prod.id === product.id);
+
+    if (productInCart === undefined) {
+      this.cart.push({ product, count: 1 });
+    } else {
+      productInCart.count += 1;
+    }
+  }
+
+  static removeFromCart(product: Product) {
+    this.cart = this.cart.filter(({ product: prod }) => prod.id !== product.id);
+  }
+
+  static calculateAmount() {
+    return this.cart.reduce((acc, crnt) => acc + crnt.count, 0);
+  }
+}
